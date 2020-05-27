a80fe4167e0c7782bff3f8aa78a14bb9d1e283f8
= <=>
2020-05-27 16:17:57 +0200

    return only data for created file

--- image_server/__init__.py
+++ image_server/__init__.py
@@ -5,10 +5,11 @@ from flask_jwt_extended import JWTManager
 
 def create_app():
     app = Flask(__name__)
-    app.config['STORAGE_PATH'] = '~/images'
+    app.config['STORAGE_PATH'] = '/home/malte.fillmer/images'
 
     jwt = JWTManager(app)
 
     app.register_blueprint(file.bp, url_prefix="/file")
+    app.register_blueprint(image.bp, url_prefix="/image")
 
     return app
